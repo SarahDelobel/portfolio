ead09e8362371c1804b79449e43fe71ea15c3c6e
Sarah <99488737+SarahDelobel@users.noreply.github.com>
2022-02-14 15:39:51 +0100

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,6 @@
 var myFullpage = new fullpage('#fullpage', {
     lockAnchors: false,
-    anchors:['app-hero-banner', 'secondPage'],
+    anchors:['app-hero-banner', 'app-about', 'app-soft-skills','app-hard-skills'],
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: [],
