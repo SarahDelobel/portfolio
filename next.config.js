75bac8b848da6b4bd56366b6e635331bff5f2500
SarahDelobel <sasunah@outlook.fr>
2022-02-14 02:16:51 +0100

    changement

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/SarahDelobel/sarahdelobel.github.io/portfolio/' : '',
+
+}
+
