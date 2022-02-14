3c446d41fd47c73dd5e34011ddaf10444a9c6815
SarahDelobel <sasunah@outlook.fr>
2022-02-14 15:35:58 +0100

    ajout du site

---
+++ js/animations.js
@@ -0,0 +1,10 @@
+var myFullpage = new fullpage('#fullpage', {
+    lockAnchors: false,
+    anchors:['app-hero-banner', 'secondPage'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: [],
+    showActiveTooltip: true,
+    slidesNavigation: false,
+    slidesNavPosition: 'bottom',
+});
